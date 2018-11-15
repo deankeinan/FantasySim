58bb71e0dbaac9dbf55f3c4218bad2a702457495
Dean Keinan <deank022@gmail.com>
2018-11-15 05:59:21 -0500

    added a clicker

---
+++ Assets/ClickNPCToListen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClickNPCToListen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+            try
+            {
+                if (hit.transform.name == "Bill")
+                {
+                    Debug.Log("Clicked Bill");
+                }
+            }
+            catch(NullReferenceException e){
+                Debug.Log("No colliders hit from mouse click");
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        print("hello");
+    }
+}
